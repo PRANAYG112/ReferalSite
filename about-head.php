581e257721a1c96d920dab2eb5d6bec4468834d4
PRANAYG112 <67736210+PRANAYG112@users.noreply.github.com>
2023-08-04 17:58:55 +0530

    referal site

---
+++ about-head.php
@@ -0,0 +1,82 @@
+<header>
+    <div class="main_header">
+        <div class="row">
+            <div class="col-xl-6 col-sm-12">
+                <div class="log_flex">
+                    <div class="col-lg-6 col-6">
+                        <img src="images/logo.png" alt="" width="200">
+                    </div>
+                    <div class="d-none d-sm-none d-md-block col-lg-6 col-6">
+                        <div class="search-container">
+                            <input type="text" placeholder="Search...">
+                            <button type="submit">
+                                <i class="fas fa-search"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="toggle_icon col-6">
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="text-white">☰</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-6  col-sm-12 text-white">
+                <!-- Navigation Links -->
+                <div class="row desk_nav_row">
+                    <div class="desk_menu">
+                        <div class="col-lg-9">
+                            <ul class="navigation_ul">
+                                <li><a href="index.php">Home</a></li>
+                                <li><a href="portfolio.php">Product</a></li>
+                                <li><a href="about.php">About us</a></li>
+                                <li><a href="contact.php">Contact</a></li>
+                                <li><a href="help.php">Help</a></li>
+                                <li><a href="insider.php">Insider</a></li>
+                            </ul>
+                        </div>
+
+                        <div class="col-lg-4">
+                            <a href="login.php" class="login_btn text-black">Login</a>
+                            <span>En <i class="fa-solid fa-angle-down"></i></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <ul class="navigation_ul">
+                                <li><a href="index.php">Home</a></li>
+                                <hr>
+                                <li><a href="">Product</a></li>
+                                <hr>
+                                <li><a href="about.php">About us</a></li>
+                                <hr>
+                                <li><a href="contact.php">Contact</a></li>
+                                <hr>
+                                <li><a href="">Help</a></li>
+                                <hr>
+                                <li><a href="">Insiders</a></li>
+                            </ul>
+                        </div>
+                        <!-- <div class="row p-3"> -->
+                        <!-- <div class="search-container col-6">
+                                <input type="text" placeholder="Search...">
+                                <button type="submit">
+                                    <i class="fas fa-search"></i>
+                                </button>
+                            </div> -->
+                        <div class="col-6">
+                            <button class="login_btn">Login</button>
+                            <span>En <i class="fa-solid fa-angle-down"></i></span>
+                        </div>
+                        <!-- </div> -->
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
